0226ae0c90607c85d514df1f65224195cb49e87a
yotamg6 <93833968+yotamg6@users.noreply.github.com>
2022-03-22 19:55:27 +0200

    test responsiveness

--- loop-machine/src/components/Track.js
+++ loop-machine/src/components/Track.js
@@ -2,45 +2,45 @@ import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
 import CardContent from "@mui/material/CardContent";
 
 import Box from "@mui/material/Box";
 
 const Track = ({ track: { title, isMuted }, index, onMutePress, style }) => {
   return (
-    <Card
-      sx={{
-        backgroundColor: style,
-        width: "40%",
-        height: "20%",
-        m: 2,
-      }}
-    >
-      <Box sx={{ m: 1 }}>
-        <CardContent>
-          <p className="trackTitle">Track: {title}</p>
-        </CardContent>
-        <Box>
-          {isMuted ? (
-            <Button
-              onClick={() => onMutePress(index)}
-              variant="contained"
-              startIcon={<VolumeUpIcon />}
-            >
-              UnMute
-            </Button>
-          ) : (
-            <Button
-              onClick={() => onMutePress(index)}
-              variant="contained"
-              startIcon={<VolumeOffIcon />}
-            >
-              Mute
-            </Button>
-          )}
+    <Grid style={{ width: "40%", height: "25%" }}>
+      <Card
+        sx={{
+          backgroundColor: style,
+        }}
+      >
+        <Box sx={{ m: 1 }}>
+          <CardContent>
+            <p className="trackTitle">Track: {title}</p>
+          </CardContent>
+          <Box>
+            {isMuted ? (
+              <Button
+                onClick={() => onMutePress(index)}
+                variant="contained"
+                startIcon={<VolumeUpIcon />}
+              >
+                UnMute
+              </Button>
+            ) : (
+              <Button
+                onClick={() => onMutePress(index)}
+                variant="contained"
+                startIcon={<VolumeOffIcon />}
+              >
+                Mute
+              </Button>
+            )}
+          </Box>
         </Box>
-      </Box>
-    </Card>
+      </Card>
+    </Grid>
   );
 };
 
